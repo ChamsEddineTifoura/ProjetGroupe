f526582a6a7bf3d5aa84f8001936b830e00df2fc
ChamsEddineTifoura <chams_tifoura@hotmail.fr>
2022-05-04 11:31:41 +0200

    Modif du fichier FilmType.php pour relier avec themoviedb

--- src/Form/FilmType.php
+++ src/Form/FilmType.php
@@ -7,15 +7,39 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class FilmType extends AbstractType
 {
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $key = "2630e5d421793226b64f8e6f65bcf6e8";
+
+        $json = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?api_key=$key&language=fr");
+
+        $result = json_decode($json, true);
+
+        $genres = $result;
+
         $builder
-            ->add('name', TextType::class)
-            ->add('category', TextType::class)
+
+            ->add('category', ChoiceType::class, [
+                'choices'  => [
+                    $genres['genres'][0]['name'] => $genres['genres'][0]['name'],
+                    $genres['genres'][1]['name'] => $genres['genres'][1]['name'],
+                    $genres['genres'][2]['name'] => $genres['genres'][2]['name'],
+                ], 
+            ])
+            ->add('name', ChoiceType::class, [
+                'choices'  => [
+                    'Le seigneur des Anneaux' => 'Le seigneur des Anneaux',
+                    'Star Wars' => 'Star Wars',
+                    'Taxi' => 'Taxi',
+                ],
+            ])
             ->add('synopsis', TextType::class)
             ->add('image', TextType::class)
             ->add('submit', SubmitType::class, [
