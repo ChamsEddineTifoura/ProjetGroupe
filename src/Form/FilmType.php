252c904fb3cd1f5ad25632309b15ef464b30bcc8
Fujiwaara <andreizerdane@gmail.com>
2022-05-08 21:20:11 +0200

    remplacement champs select par barre de recherche pour le formulaire d'enregistrement de film

--- src/Form/FilmType.php
+++ src/Form/FilmType.php
@@ -16,32 +16,32 @@ class FilmType extends AbstractType
 {   
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $key = "2630e5d421793226b64f8e6f65bcf6e8";
-        $json = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?api_key=$key&language=fr");
-        $result = json_decode($json, true);
+        // $key = "2630e5d421793226b64f8e6f65bcf6e8";
+        // $json = file_get_contents("https://api.themoviedb.org/3/genre/movie/list?api_key=$key&language=fr");
+        // $result = json_decode($json, true);
 
-        $genres = array();
+        // $genres = array();
 
-        for ($i=0; $i < count($result["genres"]); $i++) { 
-            $genre = new Genre();
-            $genre->setId($result["genres"][$i]['id']);
-            $genre->setName($result["genres"][$i]['name']);
-            $genres[] = $genre;
-        }
+        // for ($i=0; $i < count($result["genres"]); $i++) { 
+        //     $genre = new Genre();
+        //     $genre->setId($result["genres"][$i]['id']);
+        //     $genre->setName($result["genres"][$i]['name']);
+        //     $genres[] = $genre;
+        // }
         //dd($genres);
         $builder
-            ->add('category', ChoiceType::class, [
-                'placeholder' => 'Choisissez une catégorie',
-                'choices'  => $genres, 
-                'choice_value' => 'id',
-                'choice_label' => function(?Genre $genre) {
-                    return $genre ? $genre->getName() : '';
-                },
+            // ->add('category', ChoiceType::class, [
+            //     'placeholder' => 'Choisissez une catégorie',
+            //     'choices'  => $genres, 
+            //     'choice_value' => 'id',
+            //     'choice_label' => function(?Genre $genre) {
+            //         return $genre ? $genre->getName() : '';
+            //     },
+            // ])
+            ->add('name', TextType::class, [
+                'label'  => 'Rechercher un film',
             ])
-            ->add('name', ChoiceType::class, [
-                'choices'  => [],
-                'choice_value' => 'id',
-            ])
+            ->add('category', TextType::class)
             ->add('synopsis', TextType::class, [
                 'attr' => [
                     'maxlength' => 255,
